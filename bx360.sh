7a58f1e7190cae12c28bc28f552e36cfaf8b1f89
Raymond Aguilar <flexgrip@arthur-dell.(none)>
2009-10-18 16:28:25 -0500

    Modified the bx360.sh script so that it quits save that stupid .html file that wget downloads while doing the xbmc notification.

--- bx360.sh
+++ bx360.sh
@@ -2,4 +2,4 @@
 
 growisofs -use-the-force-luke=dao -use-the-force-luke=break:1913760  -dvd-compat -speed="$3" -Z "$1"="$2"
 
-wget "http://$5/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(Finished!,360%20backup%20complete...,5000,$4))"
+wget --spider "http://$5/xbmcCmds/xbmcHttp?command=ExecBuiltIn(Notification(Finished!,360%20backup%20complete...,5000,$4))"
